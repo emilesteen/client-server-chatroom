fd56cce64ec1c7730615895f375ab8e296c2e708
Emile Steenkamp <emile@steenkamps.org>
2019-09-05 17:25:06 +0200

    gui working

--- client/client.go
+++ client/client.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"github.com/marcusolsson/tui-go"
 	"log"
 	"net"
 	"os"
+	"sync"
 )
 
 const (
@@ -13,17 +13,70 @@ const (
 )
 
 var buf [512]byte
+var lock sync.RWMutex
 
-func client(ip string) {
+func startClientUI(ip string) {
 	conn := openConnection(ip)
+	ui, messageArea := initUI(conn)
 
-	// Receive welcome message
-	message := receiveMessage(conn)
-	fmt.Print(message)
-
-	go sendMessageRoutine(conn)
-	receiveMessageRoutine(conn)
+	go uiReceiveMessagesRoutine(conn, ui, messageArea)
+	if err := ui.Run(); err != nil {
+		log.Fatal(err)
+	}
 	closeConnection(conn)
+}
+
+func initUI(conn net.Conn) (tui.UI, *tui.Box) {
+	userList := tui.NewVBox(tui.NewLabel("User list:"), tui.NewSpacer())
+	userList.SetBorder(true)
+
+	messageArea := tui.NewVBox()
+	messageAreaScroll := tui.NewScrollArea(messageArea)
+	messageAreaBox := tui.NewVBox(messageAreaScroll)
+	messageAreaBox.SetBorder(true)
+
+	input := tui.NewEntry()
+	input.SetFocused(true)
+	input.SetSizePolicy(tui.Expanding, tui.Maximum)
+
+	inputBox := tui.NewHBox(input)
+	inputBox.SetBorder(true)
+	inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+
+	chat := tui.NewVBox(messageAreaBox, inputBox)
+	chat.SetSizePolicy(tui.Expanding, tui.Expanding)
+
+	input.OnSubmit(func(e *tui.Entry) {
+		message := e.Text()
+		sendMessage(conn, message)
+		input.SetText("")
+	})
+
+	root := tui.NewHBox(userList, chat)
+
+	ui, err := tui.New(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ui.SetKeybinding("Esc", func() {
+		ui.Quit()
+		sendMessage(conn, "!q\n")
+	})
+
+	return ui, messageArea
+}
+
+func uiReceiveMessagesRoutine(conn net.Conn, ui tui.UI, messageArea *tui.Box) {
+	for {
+		message := receiveMessage(conn)
+		if message == "!q\n" {
+			break
+		}
+		ui.Update(func() {
+			messageArea.Append(tui.NewHBox(tui.NewLabel(message), tui.NewSpacer()))
+		})
+	}
 }
 
 func openConnection(ip string) (conn net.Conn) {
@@ -35,32 +88,6 @@ func openConnection(ip string) (conn net.Conn) {
 	}
 	log.Println("Connection open.")
 	return
-}
-
-func sendMessageRoutine(conn net.Conn) {
-	clReader := bufio.NewReader(os.Stdin)
-	for {
-		message, err := clReader.ReadString('\n')
-		if err != nil {
-			log.Println("Command line read error.")
-		}
-
-		sendMessage(conn, message)
-
-		if message == "!q\n" {
-			break
-		}
-	}
-}
-
-func receiveMessageRoutine(conn net.Conn) {
-	for {
-		message := receiveMessage(conn)
-		if message == "!q\n" {
-			break
-		}
-		fmt.Print(message)
-	}
 }
 
 func sendMessage(conn net.Conn, message string) {
@@ -90,5 +117,5 @@ func closeConnection(conn net.Conn) {
 }
 
 func main() {
-	client("127.0.0.1")
+	startClientUI("127.0.0.1")
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/emilesteen/client-server-chatroom
-
-go 1.12
